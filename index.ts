3b5c2dc3a9b86cc8d18356b3b9ab16cc35dd6213
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-04-22 00:01:11 +0900

    lesson6 完了

---
+++ index.ts
@@ -0,0 +1,10 @@
+let hello: string = 'Hello Typescript';
+console.log(hello);
+
+//lesson6 ドキュメントとしての側面を活用する
+//他の人にわかりやすくコードを説明できる
+//third patryパッケージのソースコードを読むときにtypescriptの知識がないとしんどい
+//javascriptのファイルからでもtypesceriptのパッケージをinport可能
+function add(a: number , b: number): number{
+    return a + b;
+}
