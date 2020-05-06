cdb731a76f7902143c3ed6bbdbf8f71c098299f1
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-06 18:20:07 +0900

    section5まで完了

--- TYPESCRPT-TYPE/src/interface.ts
+++ TYPESCRPT-TYPE/src/interface.ts
@@ -11,17 +11,23 @@
 //lesson66
 //readonlyなプロパティnameを持つinterfaceを継承したDeveloperクラスのnameはreadonlyでなくてよい
 
-interface Nameable{
-    name: string;
+type Nameable = {
+    name?: string;
+    //?をつけることであっても無くても良いプロパティとして指定できる
+    nickName?: string;
+}
+
+const nameable: Nameable = {
+    name: 'Quil',
+    nickName: 'Qu'
 }
 
 //Nameableインタフェースを継承した場合で、nameをHumanインタフェースで指定すると、条件によっては値が上書きされる
 //NameableインタフェースのnameにHumanインタフェースのnameプロパティを代入できるときに値が上書きされる
 interface Human extends Nameable {
-    name: string;
     age: number;
     //greeting: (message: string) => void;
-    greeting (message: string): void;
+    greeting(message: string): void;
 } 
 
 const human = {
@@ -33,16 +39,15 @@ const human = {
 }
 
 class Developer implements Human {
-    constructor(public name: string, public age: number, public experience: number){
-
+    constructor(public age: number, public experience: number, public name?: string){
     }
     greeting(message: string){
-        console.log('hello');
+        console.log(message);
     }
 }
 
 //少なくともHuman型のプロパティを備えていることはわかる
-const user: Human = new Developer('Mikel', 35, 5);
+//const user: Human = new Developer('Mikel', 35, 5);
 
 //Lesson68インタフェースでも関数の型を表現できる
 //基本的にはtypeを用いて関数の型を表現する
@@ -60,3 +65,17 @@ addFunc = (n1: number, n2: number) => {
 //addFunc = (n1: number, n2: number) => {
     //return n1 + n2;
 //}
+
+const tmpDeveloper = {
+    name: 'Quill',
+    age: 38,
+    experience: 3,
+    greeting(message: string) {
+      console.log(message);
+    }
+  }
+  const user: Human = new Developer(38, 3);
+  if (user.name) {
+    user.name.toUpperCase()
+  }
+  console.log(user.name);
