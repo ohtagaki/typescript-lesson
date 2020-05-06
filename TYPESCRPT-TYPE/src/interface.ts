ffad44d4c14ae93a14af350bdd394c2b8d32f342
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-06 17:34:44 +0900

    Lesson65完了

--- TYPESCRPT-TYPE/src/interface.ts
+++ TYPESCRPT-TYPE/src/interface.ts
@@ -5,6 +5,10 @@
 
 //Lesson64 implements
 
+//Lesson65
+//構造的部分型：intterfaceが持つプロパティを保持しつつ、新たなフィールドを入れてもエラーが出ない
+
+
 interface Human {
     name: string;
     age: number;
@@ -22,9 +26,12 @@ const human = {
 
 class Developer implements Human {
     constructor(public name: string, public age: number, public experience: number){
-        
+
     }
     greeting(message: string){
         console.log('hello');
     }
 }
+
+//少なくともHuman型のプロパティを備えていることはわかる
+const user: Human = new Developer('Mikel', 35, 5);
