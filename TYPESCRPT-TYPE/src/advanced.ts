239a910f111ca5c5e13ac3732c9441e8082f5067
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-05-08 00:47:01 +0900

    Lesson73完了

--- TYPESCRPT-TYPE/src/advanced.ts
+++ TYPESCRPT-TYPE/src/advanced.ts
@@ -23,3 +23,44 @@ type tmp = string & number;
 type NumberBoolean = number | boolean;
 type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber;
+
+//Lesson73
+function toUpperCase(x: string | number){
+    if(typeof x === 'string'){
+        return x.toUpperCase();
+    }
+    return '';
+}
+
+type NomadWorker = Engineer | Blogger;
+function describeNomadWorkerProfile(nomadWorker: NomadWorker){
+    console.log(nomadWorker.name);
+    if('role' in nomadWorker){
+        console.log(nomadWorker.role);
+    }
+    if('follower' in nomadWorker){
+        console.log(nomadWorker.follower);
+    }
+}
+
+class Dog {
+    speak(){
+        console.log('bow-bow');
+    }
+}
+class Bird {
+    speak(){
+        console.log('tweet-tweet');
+    }
+    fly(){
+        console.log('flutter');
+    }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet){
+    pet.speak();
+    if(pet instanceof Bird){
+        pet.fly();
+    }
+}
