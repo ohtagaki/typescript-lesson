ff1341e037d3fa7b40a3cb39822a97aafa491c25
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-08 01:10:13 +0900

    lessn15までの復讐完了

---
+++ TYPESCRPT-TYPE/index.ts
@@ -0,0 +1,13 @@
+//lesson14
+let hasValue: boolean = true;
+let count: number = 15;
+let floatCount: number = 1.25;
+let negativeCount: number = -20;
+
+let single: string = 'Hello';
+let double: string = "Hello";
+let back: string = `Hello`;
+
+//lesson15
+let expectValue = false;
+let hello;
