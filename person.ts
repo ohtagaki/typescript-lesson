8ca557aeaac0280035aecdbecea35f1135fe78a1
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-08 01:12:57 +0900

    復習のためいったん削除

--- person.ts
+++
@@ -1,8 +0,0 @@
-//lesson8 Linterとしての側面を活用する
-//JavaScript・・・実行時エラーとして出る
-//TypeScript・・・実行前にコンパイルエラーとして出る
-//→機械の目を通してcheckできるため、コード量が増えるほど効力増す
-//TypeScriptコンパイラが搭載されているエディタを使うことでリアルタイムで確認できる
-//他のLinterには他のLinterの良さがある
-//エラーが出ていても、TypeScriptのファイルをJavaScriptファイルとしてコンパイルすることは可能
-console.log(person);
