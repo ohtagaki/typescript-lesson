2c6a5c9ed0759945f248c2c3c726868883dcd701
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-11 00:46:29 +0900

    lesson53完了

--- TYPESCRPT-TYPE/src/class.ts
+++ TYPESCRPT-TYPE/src/class.ts
@@ -1,7 +1,11 @@
 class Person {
 
-    constructor(public name: string, private age: number) {
+    readonly id: number = 32;
+    constructor(public readonly name: string, private age: number) {
         //初期化
+        this.id = 31;
+        this.name = 'hiroki';
+        this.id = 30;
     }
 
     greeting(this: Person){
@@ -17,6 +21,7 @@ const mike = new Person('Mike', 24);
 console.log(mike);
 mike.incrementAge();
 mike.greeting();
+console.log(mike.id);
 
 // const anotherMike = {
 //     name: 'anotherMike',
