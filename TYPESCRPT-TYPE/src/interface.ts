0abc6c185d75dfcf76f89163e91854a3d5eb601e
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-11 14:21:12 +0900

    lessson64完了

--- TYPESCRPT-TYPE/src/interface.ts
+++ TYPESCRPT-TYPE/src/interface.ts
@@ -12,3 +12,11 @@ const human: Human ={
     }
 }
 
+class Developer implements Human {
+    constructor(public name: string, public age: number, public career:number){
+
+    }
+    greeting(message: string) {
+        console.log(message);
+    }
+}
