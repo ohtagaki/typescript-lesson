c9b28f81a95734403fbfef3aed6dd2913c53968a
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-12 23:53:32 +0900

    lesson73完了

--- TYPESCRPT-TYPE/src/advanced.ts
+++ TYPESCRPT-TYPE/src/advanced.ts
@@ -24,3 +24,54 @@ type tmp = string & number;
 type numberBoolean = number | boolean;
 type stringNumber = string | number;
 type mixType = numberBoolean & stringNumber;
+
+//lesson73
+function toUpperCase(x : string | number) {
+    if(typeof x === 'string') {
+        return x.toUpperCase();
+    }else{
+        return '';
+    }
+}
+
+type NomadWorker = Engineer | Blogger;
+function describeNomadWorkerProfile(nomadWorker: NomadWorker) {
+    console.log(nomadWorker.name);
+    //typeof演算子が使えるのは7つの型のみ
+    //プロパティがあるかどうかのcheckはinを使う
+    if('role' in nomadWorker) {
+        //Engineer型であることが保証されている
+        console.log(nomadWorker.role);
+    }
+
+    if('follower' in nomadWorker) {
+        //Blogger型であることが保証されている
+        console.log(nomadWorker.follower);
+    }
+}
+
+class Dog {
+    speak() {
+        console.log('bow-bow');
+    }
+}
+
+class Bird {
+    speak() {
+        console.log('tweet-tweet');
+    }
+    fly() {
+        console.log('flutter');
+    }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+    pet.speak();
+    //instanceof：生成されたインスタンスがどのclass(設計図)から生成されたかをcheckする
+    //もともと、JavaScriptに搭載されている
+    if(pet instanceof Bird){
+        pet.fly();
+    }
+}
+havePet(new Bird());
