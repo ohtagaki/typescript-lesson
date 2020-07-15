aeefe25822a9b70e8f976c73a356d8c9261b064e
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-15 22:44:30 +0900

    lesson98完了

--- TYPESCRPT-TYPE/src/generics.ts
+++ TYPESCRPT-TYPE/src/generics.ts
@@ -41,3 +41,20 @@ const tmpDatabase: TmpDatabase<number> = {
     id: 32,
     data: [35]
 }
+
+interface Todo {
+    title: string;
+    text: string;
+}
+type Todoable = Partial<Todo>
+type ReadTodo = Readonly<Todo>
+
+const fetchData: Promise<string> = new Promise(resolve => {
+    setTimeout(() => {
+        resolve('hello');
+    }, 3000);
+})
+fetchData.then(data => {
+    data.toUpperCase();
+})
+const vegetables: Array<string> = ['Tomato', 'Apple', 'Banana'];
