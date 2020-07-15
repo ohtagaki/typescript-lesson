967c4603552efaa714cd3d03e1ac1851957c9909
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-15 22:04:31 +0900

    lesson95完了

--- TYPESCRPT-TYPE/src/generics.ts
+++ TYPESCRPT-TYPE/src/generics.ts
@@ -1,7 +1,11 @@
 //lesson93
 //型を引数として受け取る
-function copy<T extends {name: string}> (value: T): T{
+function copy<T extends {name: string}, U extends keyof T> (value: T, key: U): T{
+    value[key]
     return value;
 }
 
-console.log(copy({name: 'Mike'}));
+console.log(copy({name: 'Mike' , age: 23}, 'name'));
+
+//型のkeyを取り出してunion演算子をつける
+type K = keyof { name: string; age: number}
