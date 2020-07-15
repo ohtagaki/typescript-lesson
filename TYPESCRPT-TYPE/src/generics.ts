e529695515212512b31e216a25ea0e6cf93e52e1
ohtagaki <sweetlife0815hiikor@gmail.com>
2020-07-15 22:18:33 +0900

    lesson97完了

--- TYPESCRPT-TYPE/src/generics.ts
+++ TYPESCRPT-TYPE/src/generics.ts
@@ -9,3 +9,35 @@ console.log(copy({name: 'Mike' , age: 23}, 'name'));
 
 //型のkeyを取り出してunion演算子をつける
 type K = keyof { name: string; age: number}
+
+//lesson96
+class LightDatabase<T extends string | number | boolean> {
+    private data: T[] = [];
+    add(item: T){
+        this.data.push(item);
+    }
+    remove(item: T){
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get(){
+        return this.data;
+    }
+}
+
+//Union型は柔軟すぎるため注意(型安全ではなくなる)
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase.get());
+
+//interfaceにジェネリクスを適用
+interface TmpDatabase<T> {
+    id: number;
+    data: T[];
+}
+const tmpDatabase: TmpDatabase<number> = {
+    id: 32,
+    data: [35]
+}
